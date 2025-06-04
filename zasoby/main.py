699dfee1ea0cb0260d4fdccd6e969ddec8e2e98b
Patrycja Krzyżostanek <287322@student.pwr.edu.pl>
2025-06-05 00:40:57 +0200

    uruchomienie gry i zamykanie okna

--- zasoby/main.py
+++ zasoby/main.py
@@ -1,4 +1,5 @@
 from silnik import *
+import sys
 
 class Gra:
     def __init__(self):
@@ -6,4 +7,18 @@ class Gra:
         self.ekran = pg.display.set_mode(RES)
         self.zegar = pg.time.Clock()
         self.delta_czas = 1
+
+    def sprawdz_zdarzenia(self):
+        for zdarz in pg.event.get():
+            if zdarz.type == pg.QUIT or (zdarz.type == pg.KEYDOWN and zdarz.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+    
+    def graj(self):
+        while True:
+            self.sprawdz_zdarzenia()
+
+if __name__=="__main__":
+    gra=Gra()
+    gra.graj()
     
