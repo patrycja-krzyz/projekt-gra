31082cbe8ea9947bbfbf977417092bc3bcbf3cf9
Emilia Pawlak <287335@student.pwr.edu.pl>
2025-06-12 01:13:48 +0200

    dodanie klasy przedmioty i próbny przedmiot

--- zasoby/main.py
+++ zasoby/main.py
@@ -1,5 +1,6 @@
 from silnik import *
 import sys
+from przedmioty import Przedmiot 
 from gracz import *
 
 class Gra:
@@ -9,17 +10,28 @@ class Gra:
         self.zegar = pg.time.Clock()
         self.delta_czas = 1
         self.gracz = gracz(self)
-        self.tlo = pg.image.load("zasoby/tekstury/staremiasto.png").convert()
-        self.tlo = pg.transform.scale(self.tlo, RES)  
+        self.tlo = pg.image.load("tekstury/staremiasto.png").convert()
+        self.tlo = pg.transform.scale(self.tlo, RES) 
+        self.przedmioty = [
+           #Przedmiot(self,300,300, "tort.png")
+        ] 
 
     def sprawdz_zdarzenia(self):
         for zdarz in pg.event.get():
             if zdarz.type == pg.QUIT or (zdarz.type == pg.KEYDOWN and zdarz.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            elif zdarz.type == pg.KEYDOWN:
+                if zdarz.key == pg.K_e:
+                    for przedmiot in self.przedmioty:
+                        if przedmiot.sprawdz_kolizje_z_graczem() and not przedmiot.podniesiony:
+                            przedmiot.podnies()
+                            print("Przedmiot podniesiony!")
         
     def rysuj(self):
-        self.ekran.blit(self.tlo, (0, 0))  
+        self.ekran.blit(self.tlo, (0, 0)) 
+        for przedmiot in self.przedmioty:
+            przedmiot.rysuj()
         self.gracz.rysuj()                 
         pg.display.flip() 
 
---
+++ zasoby/przedmioty.py
@@ -0,0 +1,31 @@
+import pygame as pg
+import os
+from gracz import *
+
+class Przedmiot:
+    def __init__(self, gra, x, y, nazwa_pliku):
+        self.gra = gra
+        self.x = x
+        self.y = y
+        sciezka_do_obrazka = os.path.join("spritey", nazwa_pliku)
+        
+        if not os.path.exists(sciezka_do_obrazka):
+            raise FileNotFoundError(f"Nie znaleziono pliku: {sciezka_do_obrazka}")
+        
+        self.obraz = pg.image.load(sciezka_do_obrazka).convert_alpha()
+        self.obraz = pg.transform.scale(self.obraz, (40, 40))  
+        
+        self.rect = self.obraz.get_rect(topleft=(self.x, self.y))
+        self.podniesiony = False  
+
+    def rysuj(self):
+        if not self.podniesiony:
+            self.gra.ekran.blit(self.obraz, (self.x, self.y))
+    
+    def sprawdz_kolizje_z_graczem(self):
+        gracz_rect = pg.Rect(self.gra.gracz.x, self.gra.gracz.y, 50, 50)  
+        return self.rect.colliderect(gracz_rect)
+
+    def podnies(self):
+        self.podniesiony = True
+       
