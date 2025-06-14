1c33429ac06e3beccebbead52c77fee8010cc20d
Patrycja Krzyżostanek <287322@student.pwr.edu.pl>
2025-06-14 13:02:00 +0200

    system zmiany map

--- zasoby/main.py
+++ zasoby/main.py
@@ -2,33 +2,64 @@ from silnik import *
 import sys
 from przedmioty import Przedmiot 
 from gracz import *
+from mapa import *
 
-class Gra:
+class Gra: 
     def __init__(self):
         pg.init()
         self.ekran = pg.display.set_mode(RES)
         self.zegar = pg.time.Clock()
         self.delta_czas = 1
         self.gracz = gracz(self)
-        self.tlo = pg.image.load("zasoby/tekstury/staremiasto.png").convert()
+        self.tlo = pg.image.load("tekstury/staremiasto.png").convert()
+        # self.tlo = pg.image.load("zasoby/tekstury/staremiasto.png").convert()
         self.tlo = pg.transform.scale(self.tlo, RES) 
         self.przedmioty = [
-           #Przedmiot(self,300,300, "tort.png")
+           # Przedmiot(self,300,300, "tort.png")
         ] 
+        # self.mapki = [
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/staremiasto.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/dworzec.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/jakgrac.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/krzyki.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/mapa.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/Nadodrze.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/placgrunwaldzki.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/pustystarter.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/starter.png").convert(), RES),
+        #     pg.transform.scale(pg.image.load("zasoby/tekstury/zoo.png").convert(), RES),
+        # ]
         self.mapki = [
-            pg.transform.scale(pg.image.load("zasoby/tekstury/staremiasto.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/dworzec.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/jakgrac.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/krzyki.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/mapa.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/Nadodrze.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/placgrunwaldzki.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/pustystarter.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/starter.png").convert(), RES),
-            pg.transform.scale(pg.image.load("zasoby/tekstury/zoo.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/staremiasto.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/dworzec.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/jakgrac.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/krzyki.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/mapa.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/Nadodrze.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/placgrunwaldzki.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/pustystarter.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/starter.png").convert(), RES),
+            pg.transform.scale(pg.image.load("tekstury/zoo.png").convert(), RES),
         ]
-        self.indeks_mapy = 0
-        self.tlo = self.mapki[self.indeks_mapy]
+        
+        self.mapy = [
+            Mapa(0, self.mapki[0], self),
+            Mapa(1, self.mapki[1], self),
+            Mapa(2, self.mapki[2], self),
+            Mapa(3, self.mapki[3], self),
+            Mapa(4, self.mapki[4], self),
+            Mapa(5, self.mapki[5], self),
+            Mapa(6, self.mapki[6], self)
+        ]
+
+        self.mapy[0].dodaj_polaczenie("gora", 5)
+        #tu można dodać więcej połączeń i przedmioty
+
+        self.aktualna_mapa = 0
+        self.tlo = self.mapy[self.aktualna_mapa].tekstura 
+
+
+
 
     def sprawdz_zdarzenia(self):
         for zdarz in pg.event.get():
@@ -41,7 +72,8 @@ class Gra:
                         if przedmiot.sprawdz_kolizje_z_graczem() and not przedmiot.podniesiony:
                             przedmiot.podnies()
                             print("Przedmiot podniesiony!")
-        
+
+
     def rysuj(self):
         self.ekran.blit(self.tlo, (0, 0)) 
         for przedmiot in self.przedmioty:
@@ -52,6 +84,7 @@ class Gra:
     def aktualizuj(self):
         self.gracz.aktualizuj()
         self.rysuj()
+        self.mapy[self.aktualna_mapa].sprawdz_krawedzie()
         self.zegar.tick(FPS) 
 
     def graj(self):
---
+++ zasoby/mapy.py
@@ -0,0 +1,46 @@
+import pygame as pg
+from przedmioty import *
+from ustawienia import *
+
+
+marg = 20 #margines uzywany pozniej przy krawedziach mapy zeby parszywek nie wyszedl caly za okienko
+class Mapa:
+    def __init__(self, indeks, tekstura, gra):
+        self.indeks = indeks
+        self.tekstura = tekstura
+        self.gra = gra
+        self.polaczenia = {}
+    
+    def dodaj_polaczenie(self, kierunek, indeks_mapy_docelowej):
+        self.polaczenia[kierunek] = indeks_mapy_docelowej
+
+    def dodaj_przedmiot(self, przedmiot):
+        self.przedmioty.append(przedmiot)
+    
+    def sprawdz_krawedzie(self):
+        gracz = self.gra.gracz
+        #wymiary obrazka parszywka ustalone w pliku gracz.py: 70x90
+        if gracz.x < marg and "lewo" in self.polaczenia:
+            self.zmien_mape(self.polaczenia["lewo"], "lewo")
+        elif gracz.x > DLUGOSC - marg - 70 and "prawo" in self.polaczenia:
+            self.zmien_mape(self.polaczenia["prawo"], "prawo")
+        elif gracz.y < marg and "gora" in self.polaczenia:
+            self.zmien_mape(self.polaczenia["gora"], "gora")
+        elif gracz.y > WYSOKOSC - marg - 90 and "dol" in self.polaczenia:
+            self.zmien_mape(self.polaczenia["dol"], "dol")
+    
+    def zmien_mape(self, nowy_indeks, kierunek):
+
+        self.gra.aktualna_mapa = nowy_indeks
+        self.gra.tlo = self.gra.mapy[nowy_indeks].tekstura
+        
+        gracz = self.gra.gracz
+        if kierunek == "lewo":
+            gracz.x = DLUGOSC - marg - 70
+        elif kierunek == "prawo":
+            gracz.x = marg
+        elif kierunek == "gora":
+            gracz.y = WYSOKOSC - marg - 90
+        elif kierunek == "dol":
+            gracz.y = marg
+        
